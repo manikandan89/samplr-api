572dccf2d08650e1c4967e8864e474ce28b6227c
Manikandan Nagarajan <manikandan.n2010@gmail.com>
2016-04-05 18:13:09 -0400

    Rethink config

--- app/config/index.js
+++ app/config/index.js
@@ -102,7 +102,7 @@ const TEST = _.extend({}, DEVELOPMENT, {
     level: 'debug'
   },
   rethinkdb: {
-    host: process.env.RETHINKDB_PORT_29015_TCP_ADDR || 'ec2-54-186-207-41.us-west-2.compute.amazonaws.com'
+    host: process.env.RETHINKDB_PORT_29015_TCP_ADDR || 'ec2-54-186-239-231.us-west-2.compute.amazonaws.com'
   }
 });
 
